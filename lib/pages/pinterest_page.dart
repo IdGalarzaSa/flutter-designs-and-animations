38189f68f00bee7a14e27f200c0e641b8752be5a
Ivan Galarza <id.galarza.sa@gmail.com>
2021-03-31 21:55:20 -0400

    basic grid design

--- lib/pages/pinterest_page.dart
+++ lib/pages/pinterest_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class PinterestPage extends StatelessWidget {
   static final routeName = "PinterestPage";
@@ -7,7 +8,46 @@ class PinterestPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Hola mundo")),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: PinterestGrid(),
+      ),
+    );
+  }
+}
+
+class PinterestGrid extends StatelessWidget {
+  final List<int> itemList = List.generate(200, (index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return new StaggeredGridView.countBuilder(
+      crossAxisCount: 4,
+      itemCount: itemList.length,
+      itemBuilder: (BuildContext context, int index) => _PinterestItem(index),
+      staggeredTileBuilder: (int index) => new StaggeredTile.count(2, index.isEven ? 2 : 3),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+    );
+  }
+}
+
+class _PinterestItem extends StatelessWidget {
+  final int index;
+  _PinterestItem(this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(color: Colors.teal, borderRadius: BorderRadius.all(Radius.circular(25))),
+      child: new Center(
+        child: new CircleAvatar(
+          backgroundColor: Colors.white,
+          child: new Text('$index'),
+        ),
+      ),
     );
   }
 }
