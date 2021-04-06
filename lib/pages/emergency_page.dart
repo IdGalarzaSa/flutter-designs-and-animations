c2a66e900e5e1e25c300550d55e52c27f4a7877a
Ivan Galarza <id.galarza.sa@gmail.com>
2021-04-06 15:43:29 -0400

    Emergency page finished

--- lib/pages/emergency_page.dart
+++ lib/pages/emergency_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_designs/widgets/headers.dart';
 import 'package:flutter_designs/widgets/wide_button_with_icon.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animate_do/animate_do.dart';
 
 class EmergencyPage extends StatelessWidget {
   static final routeName = "EmergencyPage";
@@ -10,14 +11,14 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: WideButtonWithIcon(
-          text: 'Motor Accident',
-          icon: FontAwesomeIcons.carCrash,
-          gradientColor1: Color(0xff6989F5),
-          gradientColor2: Color(0xff906EF5),
-          onPress: () => print("Click"),
-        ),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 200),
+            child: _WideButtonList(),
+          ),
+          _HeaderPage(),
+        ],
       ),
     );
   }
@@ -26,12 +27,156 @@ class EmergencyPage extends StatelessWidget {
 class _HeaderPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return HeaderWithIcon(
-      icon: FontAwesomeIcons.plus,
-      subTitle: 'Haz solicitado',
-      title: 'Atención Médica',
-      headerColor1: Color(0xff526BF6),
-      headerColor2: Color(0xff67ACF2),
+    return Stack(
+      children: [
+        HeaderWithIcon(
+          icon: FontAwesomeIcons.plus,
+          subTitle: 'Haz solicitado',
+          title: 'Atención Médica',
+          headerColor1: Color(0xff526BF6),
+          headerColor2: Color(0xff67ACF2),
+        ),
+        Positioned(
+          right: 0,
+          top: 45,
+          child: RawMaterialButton(
+            shape: CircleBorder(),
+            onPressed: () => print("Click ellipsis"),
+            child: Icon(FontAwesomeIcons.ellipsisV),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _WideButtonList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        SizedBox(
+          height: 85,
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Motor Accident',
+            icon: FontAwesomeIcons.carCrash,
+            gradientColor1: Color(0xff6989F5),
+            gradientColor2: Color(0xff906EF5),
+            onPress: () => print("Click"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Medical Emergency',
+            icon: FontAwesomeIcons.plus,
+            gradientColor1: Color(0xff66A9F2),
+            gradientColor2: Color(0xff536CF6),
+            onPress: () => print("Click 2"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Theft / Harrasement',
+            icon: FontAwesomeIcons.theaterMasks,
+            gradientColor1: Color(0xffF2D572),
+            gradientColor2: Color(0xffE06AA3),
+            onPress: () => print("Click 3"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Awards',
+            icon: FontAwesomeIcons.biking,
+            gradientColor1: Color(0xff317183),
+            gradientColor2: Color(0xff46997D),
+            onPress: () => print("Click 4"),
+          ),
+        ),
+        FadeInLeft(
+          child: WideButtonWithIcon(
+            text: 'Motor Accident',
+            icon: FontAwesomeIcons.carCrash,
+            gradientColor1: Color(0xff6989F5),
+            gradientColor2: Color(0xff906EF5),
+            onPress: () => print("Click"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Medical Emergency',
+            icon: FontAwesomeIcons.plus,
+            gradientColor1: Color(0xff66A9F2),
+            gradientColor2: Color(0xff536CF6),
+            onPress: () => print("Click 2"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Theft / Harrasement',
+            icon: FontAwesomeIcons.theaterMasks,
+            gradientColor1: Color(0xffF2D572),
+            gradientColor2: Color(0xffE06AA3),
+            onPress: () => print("Click 3"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Awards',
+            icon: FontAwesomeIcons.biking,
+            gradientColor1: Color(0xff317183),
+            gradientColor2: Color(0xff46997D),
+            onPress: () => print("Click 4"),
+          ),
+        ),
+        FadeInLeft(
+          child: WideButtonWithIcon(
+            text: 'Motor Accident',
+            icon: FontAwesomeIcons.carCrash,
+            gradientColor1: Color(0xff6989F5),
+            gradientColor2: Color(0xff906EF5),
+            onPress: () => print("Click"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Medical Emergency',
+            icon: FontAwesomeIcons.plus,
+            gradientColor1: Color(0xff66A9F2),
+            gradientColor2: Color(0xff536CF6),
+            onPress: () => print("Click 2"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Theft / Harrasement',
+            icon: FontAwesomeIcons.theaterMasks,
+            gradientColor1: Color(0xffF2D572),
+            gradientColor2: Color(0xffE06AA3),
+            onPress: () => print("Click 3"),
+          ),
+        ),
+        FadeInLeft(
+          duration: Duration(milliseconds: 250),
+          child: WideButtonWithIcon(
+            text: 'Awards',
+            icon: FontAwesomeIcons.biking,
+            gradientColor1: Color(0xff317183),
+            gradientColor2: Color(0xff46997D),
+            onPress: () => print("Click 4"),
+          ),
+        ),
+      ],
     );
   }
 }
